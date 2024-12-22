e1c430eadfcd16a317a5b818da921c80468fa301
Cameron Aaron <cameronaaron@users.noreply.github.com>
2024-12-22 11:51:14 -0800

    unarchive on message send

--- lib/services/backend/action_handler.dart
+++ lib/services/backend/action_handler.dart
@@ -148,6 +148,7 @@ class ActionHandler extends GetxService {
   }
 
   Future<void> sendMessage(Chat c, Message m, Message? selected, String? r) async {
+    if (c.isArchived!) c.toggleArchived(false);
     final completer = Completer<void>();
     if (r == null) {
       backend.sendMessage(c, m).then((newMessage) async {
